465b9cc4843fa615a5a40455deac70e25094c370
Jaehwany <eejh22@gmail.com>
2022-08-27 04:30:44 +0900

    Folder Divide

--- Web/1. JSP 프로그래밍/2.login_ex/1.login/src/com/study/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.study.service;
-
-import com.study.dto.LoginDTO;
-
-public interface LoginService {
-	public LoginDTO getLoginUser(String id, String pwd);
-}
--- Web/1. JSP 프로그래밍/4.bookshop_proj/2.BookShop_eljstl/src/com/book/dao/LoginDao.java
+++
@@ -1,5 +0,0 @@
-package com.book.dao;
-
-public interface LoginDao {
-	public boolean getLoginUser(String userid, String userpwd);
-}
